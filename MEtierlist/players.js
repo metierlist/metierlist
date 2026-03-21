0b75f61254962c50982e5ab3af53a55df7315209
metierlist <sreyfisch98@gmail.com>
2026-03-21 09:15:54 +0500

    Update players.js

--- MEtierlist/players.js
+++ MEtierlist/players.js
@@ -37,4 +37,5 @@ const players = [
 {name:"_SpookyLegend", region:"AS", kits:["Axe"], tiers:["LT4"]},
 {name:"_SpookyLegend", region:"AS", kits:["NethOP"], tiers:["LT4"]},
 {name:"Streyfe", region:"AS", kits:["Vanilla"], tiers:["HT4"]},
+{name:"RaspyYT", region:"AS", kits:["NethOP"], tiers:["LT4"]},
 ];
