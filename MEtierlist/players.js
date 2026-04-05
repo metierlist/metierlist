2645a674d5c9c5e52435c073a3b88e6102d14755
metierlist <sreyfisch98@gmail.com>
2026-04-05 20:17:03 +0500

    Update players.js

--- MEtierlist/players.js
+++ MEtierlist/players.js
@@ -57,4 +57,6 @@ const players = [
 {name:"FlameFragse", region:"AS", kits:["Sword"], tiers:["LT3"]},
 {name:"Podina69", region:"AS", kits:["Axe"], tiers:["LT4"]},
 {name:"Dr4gonis", region:"AS", kits:["Mace"], tiers:["LT3"]},
+{name:"Dr4gonis", region:"AS", kits:["UHC"], tiers:["LT3"]},
+{name:"mysticxxx", region:"AS", kits:["UHC"], tiers:["HT5"]},
 ];
