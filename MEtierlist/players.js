8551ac0e10999159ea0cca05949ce8d582029168
metierlist <sreyfisch98@gmail.com>
2026-04-18 17:49:32 +0500

    Update players.js

--- MEtierlist/players.js
+++ MEtierlist/players.js
@@ -97,4 +97,5 @@ const players = [
 {name:"Aunraza", region:"AS", kits:["Axe"], tiers:["HT5"]},
 {name:"Zaidx1ksubs", region:"AS", kits:["Axe"], tiers:["LT3"]},
 {name:"Deadliest_Bladez", region:"AS", kits:["SMP"], tiers:["HT5"]},
+{name:"Zaidx1ksubs", region:"AS", kits:["SMP"], tiers:["LT3"]},
 ];
